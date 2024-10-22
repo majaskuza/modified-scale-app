810661fe4f61a4a69bf533c96a6886c733a28d26
majaskuza <158510150+majaskuza@users.noreply.github.com>
2024-10-22 08:18:40 +0100

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -14,6 +14,7 @@ class zmq_messenger(object):
         self.zmq_pusher = zmqhelper.pusher()
         self.zmq_conn = True
 
+'''(Maja) not calling this function anymore
     def start_session(self,subjid,rigid):
         now = datetime.now()
         current_ts = now.strftime("%Y-%m-%d_%H:%M:%S")
@@ -22,6 +23,6 @@ class zmq_messenger(object):
         time.sleep(0.2)
         self.zmq_pusher.send_string(f'{rigid}_startsession {msg}')
         time.sleep(0.2)
-        self.zmq_pusher.send_string(f'{rigid}_startsession {msg}')
+        self.zmq_pusher.send_string(f'{rigid}_startsession {msg}')'''
 
 
