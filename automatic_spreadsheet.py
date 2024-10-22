ee121668b1c268dc41441223499e4795622c9390
majaskuza <158510150+majaskuza@users.noreply.github.com>
2024-10-22 09:13:53 +0100

    Update automatic_spreadsheet.py

--- automatic_spreadsheet.py
+++ automatic_spreadsheet.py
@@ -1,29 +1,38 @@
+import pandas as pd
 
-#saving the output of the scale recording into a google spread sheet
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+def create_general_info_spreadsheet(url):
+    general_info_data = {
+        'Cage': [],
+        'ID': [],
+        'Name': [],
+        'Start weight': [],
+        '80% weight': []
+    }
+    general_info_df = pd.DataFrame(general_info_data)
+    general_info_df.to_excel(url, index=False, engine='openpyxl')
 
-# Define the scope of the application
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+def add_recording_entry(url, subjid, training_start, weight, animal_fed_at, training_end):
+    try:
+        df = pd.read_excel(url, engine='openpyxl')
+    except FileNotFoundError:
+        df = pd.DataFrame(columns=['Date', 'Subjid', 'Training Start', 'Training End', 'Animal Fed At', 'Food Amount', 'Percent', 'Weight'])
+    percent = 0.8 * weight
+    new_entry = {
+        'Date': pd.Timestamp.now().date(),
+        'Subjid': subjid,
+        'Training Start': training_start,
+        'Training End': training_end,
+        'Animal Fed At': '', #  blank for manual entry
+        'Food Amount': '',  
+        'Percent': percent,
+        'Weight': weight
+    }
+    df = df.append(new_entry, ignore_index=True)
+    df.to_excel(url, index=False, engine='openpyxl')
 
-# Load the credentials from the JSON key file
-creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/credentials.json', scope)
 
-# Authorize the gspread client
-client = gspread.authorize(creds)
 
-# Open the Google Sheet by name
-sheet = client.open("Your Google Sheet Name").sheet1  # You can also use .get_worksheet(index) if you have multiple sheets
-
-# Example of writing data to the sheet
-data = [
-    ["Weight", "Time"],  # Headers
-    [25.3, "2024-10-14 10:00"],  # Sample data
-    [30.1, "2024-10-14 10:05"],
-]
-for row in data:
-    sheet.append_row(row)  # Append each row to the Google Sheet
-
-# Example of reading data from the sheet
-all_data = sheet.get_all_records()  # Get all records as a list of dictionaries
-print(all_data)  # Print the data read from the Google Sheet
+#example usage of above functions
+url = "https://liveuclac.sharepoint.com/sites/swcpil/_layouts/15/Doc.aspx?sourcedoc=%7BB059A3FD-2266-4A44-986E-DAF8D4305725%7D&file=Edmund%20-%20automatic%20weight.xlsx"
+create_general_info_spreadsheet(url)
+add_recording_entry(url, subjid='HAA-1105431', training_start='2024-10-22', weight=320, animal_fed_at='', training_end='')
