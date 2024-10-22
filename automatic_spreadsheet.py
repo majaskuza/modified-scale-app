9e0e585fae6f1ab06588fcc89b16d1070b91c369
majaskuza <158510150+majaskuza@users.noreply.github.com>
2024-10-22 09:24:37 +0100

    Update automatic_spreadsheet.py

--- automatic_spreadsheet.py
+++ automatic_spreadsheet.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 def create_general_info_spreadsheet(url):
     general_info_data = {
@@ -11,28 +12,29 @@ def create_general_info_spreadsheet(url):
     general_info_df = pd.DataFrame(general_info_data)
     general_info_df.to_excel(url, index=False, engine='openpyxl')
 
-def add_recording_entry(url, subjid, training_start, weight, animal_fed_at, training_end):
-    try:
-        df = pd.read_excel(url, engine='openpyxl')
-    except FileNotFoundError:
-        df = pd.DataFrame(columns=['Date', 'Subjid', 'Training Start', 'Training End', 'Animal Fed At', 'Food Amount', 'Percent', 'Weight'])
-    percent = 0.8 * weight
+#function to add the weight information from a single session into the excel spreedsheet
+def add_recording_entry(url, subjid, weight, animal_fed_at='', training_end='', food_amount=''):
+    timestamp = datetime.now().strftime("%H:%M")  
+    percent = 0.8 * weight  
     new_entry = {
-        'Date': pd.Timestamp.now().date(),
+        'Date': datetime.now().strftime("%Y-%m-%d"),  
         'Subjid': subjid,
-        'Training Start': training_start,
+        'Training Start': timestamp,
         'Training End': training_end,
-        'Animal Fed At': '', #  blank for manual entry
-        'Food Amount': '',  
+        'Animal Fed At': animal_fed_at,
+        'Food Amount': food_amount,
         'Percent': percent,
         'Weight': weight
     }
+    try:
+        df = pd.read_excel(url)
+    except FileNotFoundError:
+        df = pd.DataFrame(columns=new_entry.keys())  # Create an empty DataFrame with the columns
     df = df.append(new_entry, ignore_index=True)
-    df.to_excel(url, index=False, engine='openpyxl')
+    df.to_excel(url, index=False)
 
-
+  
 
 #example usage of above functions
-url = "https://liveuclac.sharepoint.com/sites/swcpil/_layouts/15/Doc.aspx?sourcedoc=%7BB059A3FD-2266-4A44-986E-DAF8D4305725%7D&file=Edmund%20-%20automatic%20weight.xlsx"
-create_general_info_spreadsheet(url)
-add_recording_entry(url, subjid='HAA-1105431', training_start='2024-10-22', weight=320, animal_fed_at='', training_end='')
+#url = "https://liveuclac.sharepoint.com/sites/swcpil/_layouts/15/Doc.aspx?sourcedoc=%7BB059A3FD-2266-4A44-986E-DAF8D4305725%7D&file=Edmund%20-%20automatic%20weight.xlsx"
+#create_general_info_spreadsheet(url)
